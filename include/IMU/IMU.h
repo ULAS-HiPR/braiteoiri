df5f738747781cdd791612d2393a08e2e371baa2
Derv6464 <dervlag6464@gmail.com>
2026-02-15 20:05:27 +0000

    oops not public

--- include/IMU/IMU.h
+++ include/IMU/IMU.h
@@ -3,7 +3,7 @@
 #include <data.h>
 #include "../sensor.h"
 
-class IMU : Sensor<imu_data> {
+class IMU : public Sensor<imu_data> {
 public:
     virtual bool init() = 0;
     bool update(imu_data* data) override;
